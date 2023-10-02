64c06a3dd24162fdb975371037abb89e3e74cd37
JordyV23 <jordyvargas0123@gmail.com>
2023-10-02 16:35:40 -0600

    Cambios realizados 2/10/23 - 16:34 = -Estructurado de archivos de proyecto -Implementacion de rutas para listPage e IssuePage -Modificado de template -Creacion de componente de loaderSpinner

---
+++ src/issues/pages/ListPage.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import LoaderSpinner from 'src/shared/components/LoaderSpinner.vue';
+</script>
+
+<template>
+  <div class="row q-mb-md">
+    <div class="col-12">
+      <span class="text-h4">Github Issues</span>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-xs-12 col-md-4">
+      <!-- TODO: Filtros -->
+      <!-- Filter Selector -->
+    </div>
+
+    <div class="col-xs-12 col-md-8">
+      <!-- TODO: Loader -->
+      <LoaderSpinner />
+
+      <!-- TODO: IssueList - Array IssueCard -->
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/shared/components/LoaderSpinner.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { toRef } from 'vue';
+
+interface Props {
+  title?: string;
+  color?: string;
+  size?: string;
+  thickness?: number;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  title: 'Loading',
+  color: 'primary',
+  size: '3.5rem',
+  thickness: 5,
+});
+
+const color = toRef(props, 'color');
+const size = toRef(props, 'size');
+const thickness = toRef(props, 'thickness');
+</script>
+
+<template>
+  <div class="column items-center">
+    <q-spinner :color="color" :size="size" :thickness="thickness" />
+    <span class="q-mt-sm">Loading</span>
+  </div>
+</template>
+
+<style scoped></style>
