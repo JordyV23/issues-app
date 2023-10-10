3792ec86e57bfa106efe52bf6ad943e534f8da44
JordyV23 <jordyvargas0123@gmail.com>
2023-10-09 21:13:56 -0600

    Conexion con issues personalizados

--- src/issues/pages/IssuePage.vue
+++ src/issues/pages/IssuePage.vue
@@ -2,24 +2,33 @@
 import LoaderSpinner from 'src/shared/components/LoaderSpinner.vue';
 import IssueCard from 'src/issues/components/issue-list/IssueCard.vue';
 import { useRoute } from 'vue-router';
+import useIssue from '../composables/useIssue';
 
 const route = useRoute();
 const { id = '' } = route.params;
+
+const { issueQuery } = useIssue(+id);
 </script>
 
 <template>
-  <LoaderSpinner />
   <router-link class="text-white" to="/"> Go Back</router-link>
 
   <!-- Header del Issue -->
-  <IssueCard />
+  <LoaderSpinner
+    v-if="issueQuery.isLoading.value"
+    :thickness="1"
+    size="2rem"
+    :show-text="false"
+  />
+  <IssueCard v-else-if="issueQuery.data.value" :issue="issueQuery.data.value" />
+  <p v-else>Issue with ID {{ id }} not found :c</p>
 
   <!-- Comentarios -->
   <LoaderSpinner :thickness="1" size="2rem" :show-text="false" />
 
   <div class="column">
     <span class="text-h4 q-mb-md"> Comments (5) </span>
-    <IssueCard v-for="comment of 5" :key="comment" />
+    <!-- <IssueCard v-for="comment of 5" :key="comment" /> -->
   </div>
 </template>
 
